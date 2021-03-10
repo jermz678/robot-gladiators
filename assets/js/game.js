80ed44c4ec94d55261d7b6c3f11a785cc9e52405
jermz678 <jermz678@live.com>
2021-03-09 18:42:34 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,6 +69,7 @@ var fight = function(enemyName) {
         console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining .");
 
             // check player's health
+            
             if (playerHealth <= 0){
                 window.alert(playerName + " has died! ");
                 break;
@@ -81,10 +82,17 @@ var fight = function(enemyName) {
 
 
 
-for(var i = 0; i < enemyNames.length; i++) {
-    
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-}
+        for(var i = 0; i < enemyNames.length; i++) {
+            if (playerHealth > 0) {
+                window.alert("Welcome to Robot Gladiators! Round " + (i +1 ) );
+            } else {
+                window.alert("You have lost your robot in battle! Game Over!");
+            break;
+            }
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        };
+            
+        
   
